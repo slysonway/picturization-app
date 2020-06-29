ab55cb9c28acdb389bfc3fdc4db0698d69ed9f00
Pierre <pkettmus@gmail.com>
2020-06-29 20:07:19 +0200

    add filter and FilterParameter classes

---
+++ app/src/main/java/com/esgi/picturization/data/models/Filter.kt
@@ -0,0 +1,6 @@
+package com.esgi.picturization.data.models
+
+data class Filter (
+    val name: String,
+    val parameter: List<FilterParameter>
+)
---
+++ app/src/main/java/com/esgi/picturization/data/models/FilterParameter.kt
@@ -0,0 +1,6 @@
+package com.esgi.picturization.data.models
+
+data class FilterParameter(
+    val name: String,
+    val value: String
+)
